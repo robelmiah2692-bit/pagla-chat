ac71f3844ed0874eaa3984200e3edf5e9ede2320
robelmiah2692-bit <robelmiah2692@gmail.com>
2026-02-11 10:02:10 +0400

    Update user_model.dart

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,1 +1,19 @@
+class UserModel {
+  String name;
+  String bio;
+  int diamonds;
+  int vipLevel;
 
+  UserModel({
+    required this.name,
+    required this.bio,
+    required this.diamonds,
+    this.vipLevel = 0,
+  });
+
+  // ১ লাখ ডায়মন্ডে ১ লেভেল হিসাব
+  void calculateVipLevel() {
+    vipLevel = (diamonds / 100000).floor();
+    if (vipLevel > 30) vipLevel = 30; // সর্বোচ্চ ৩০ লেভেল
+  }
+}
