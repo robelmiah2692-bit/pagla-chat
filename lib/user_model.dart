2f744cd60264a003c0c6c624bfaf920110b9c736
robelmiah2692-bit <robelmiah2692@gmail.com>
2026-02-19 00:11:13 +0400

    Update user_model.dart

--- lib/user_model.dart
+++ lib/user_model.dart
@@ -1,1 +1,17 @@
+class UserModel {
+  final String uID;
+  final String roomID;
+  final String name;
+  // ... তোমার আগের অন্য সব ভেরিয়েবল
 
+  UserModel({required this.uID, required this.roomID, required this.name});
+
+  // ডাটাবেস থেকে ডাটা পড়ার জন্য
+  factory UserModel.fromMap(Map<String, dynamic> data) {
+    return UserModel(
+      uID: data['uID'] ?? '',
+      roomID: data['roomID'] ?? '',
+      name: data['name'] ?? '',
+    );
+  }
+}
