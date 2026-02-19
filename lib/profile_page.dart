b872a87099709a6c582ff21464cab816086aba01
robelmiah2692-bit <robelmiah2692@gmail.com>
2026-02-20 01:50:31 +0400

    Update profile_page.dart

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -278,8 +278,10 @@ void _showFreeAvatars() {
                         ? const NetworkImage("https://api.dicebear.com/7.x/avataaars/png?seed=Felix")
                         : (userImageURL.startsWith('http')
                             ? NetworkImage(userImageURL) as ImageProvider
-                            : FileImage(File(userImageURL)) as ImageProvider),
-                       
+                            : (File(userImageURL).existsSync()
+                                ? FileImage(File(userImageURL)) as ImageProvider),
+                                : const NetworkImage("https://api.dicebear.com/7.x/avataaars/png?seed=Felix"))),
+                  
                     ),   
                     if (vipLevel > 0) Positioned(bottom: 0, child: Container(color: Colors.amber, child: Text(" VIP $vipLevel ", style: const TextStyle(fontSize: 10, color: Colors.black, fontWeight: FontWeight.bold)))),
                     // ছোট ক্যামেরা আইকন
