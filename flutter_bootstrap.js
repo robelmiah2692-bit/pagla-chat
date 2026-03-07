8f27dbcf0e15b7a1d7ab6c1af178572eeb91714c
robelmiah2692-bit <robelmiah2692@gmail.com>
2026-03-07 04:46:52 +0000

    Deploying to gh-pages from @ robelmiah2692-bit/pagla-chat@ebf43497bd98fb3bb7374fe85c0603c9f73f353a 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "223419229" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3451038733" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
