fe20854c813586509b84f0ee33e75a197ea9caeb
robelmiah2692-bit <robelmiah2692@gmail.com>
2026-03-13 07:59:03 +0400

    Create classic_gifts.dart

---
+++ lib/data/classic_gifts.dart
@@ -0,0 +1,4 @@
+final List<Map<String, dynamic>> classicGifts = [
+  {"id": "c1", "name": "Chocolate", "icon": "https://cdn-icons-png.flaticon.com/128/1161/1161388.png", "price": 10, "type": "classic"},
+  {"id": "c2", "name": "Teddy", "icon": "https://cdn-icons-png.flaticon.com/128/9405/9405825.png", "price": 20, "type": "classic"},
+];
