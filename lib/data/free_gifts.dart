71b130c9ab0384401d60744a68e7857a675750b6
robelmiah2692-bit <robelmiah2692@gmail.com>
2026-03-13 07:58:25 +0400

    Create free_gifts.dart

---
+++ lib/data/free_gifts.dart
@@ -0,0 +1,5 @@
+final List<Map<String, dynamic>> freeGifts = [
+  {"id": "f1", "name": "Rose", "icon": "https://cdn-icons-png.flaticon.com/128/744/744502.png", "price": 5, "type": "free"},
+  {"id": "f2", "name": "Heart", "icon": "https://cdn-icons-png.flaticon.com/128/2559/2559915.png", "price": 10, "type": "free"},
+  {"id": "f3", "name": "Coffee", "icon": "https://cdn-icons-png.flaticon.com/128/1041/1041888.png", "price": 15, "type": "free"},
+];
