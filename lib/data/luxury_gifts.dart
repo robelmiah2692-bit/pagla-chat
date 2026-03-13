3932f4d1950279a360a307f03268da9ccf9e14fc
robelmiah2692-bit <robelmiah2692@gmail.com>
2026-03-13 08:00:20 +0400

    Create luxury_gifts.dart

---
+++ lib/data/luxury_gifts.dart
@@ -0,0 +1,4 @@
+final List<Map<String, dynamic>> luxuryGifts = [
+  {"id": "l1", "name": "Sports Car", "icon": "https://cdn-icons-png.flaticon.com/128/1152/1152912.png", "price": 1000, "type": "luxury"},
+  {"id": "l2", "name": "Private Jet", "icon": "https://cdn-icons-png.flaticon.com/128/3124/3124311.png", "price": 5000, "type": "luxury"},
+];
