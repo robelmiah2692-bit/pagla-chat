6ef717640ce0057608a25a787304fe5233c73b92
robelmiah2692-bit <robelmiah2692@gmail.com>
2026-03-13 07:59:36 +0400

    Create romantic_gifts.dart

---
+++ lib/data/romantic_gifts.dart
@@ -0,0 +1,4 @@
+final List<Map<String, dynamic>> romanticGifts = [
+  {"id": "r1", "name": "Ring", "icon": "https://cdn-icons-png.flaticon.com/128/4359/4359295.png", "price": 100, "type": "romantic"},
+  {"id": "r2", "name": "Dinner", "icon": "https://cdn-icons-png.flaticon.com/128/2904/2904973.png", "price": 250, "type": "romantic"},
+];
