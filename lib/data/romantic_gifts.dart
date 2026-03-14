7fc73386e50aeedb6f5edc4eec3a446926a7261b
robelmiah2692-bit <robelmiah2692@gmail.com>
2026-03-14 08:53:21 +0400

    Update romantic_gifts.dart

--- lib/data/romantic_gifts.dart
+++ lib/data/romantic_gifts.dart
@@ -1,9 +1,10 @@
 final List<Map<String, dynamic>> romanticGifts = [
-  {"id": "r1", "name": "Ring", "icon": "https://cdn-icons-png.flaticon.com/128/4359/4359295.png", "price": 100, "type": "romantic"},
-  {"id": "r2", "name": "Dinner", "icon": "https://cdn-icons-png.flaticon.com/128/2904/2904973.png", "price": 250, "type": "romantic"},
-  {"id": "rv1", "name": "Romantic 9978", "icon": "https://cdn-icons-png.flaticon.com/128/video_icon.png", "price": 9978, "type": "romantic"},
-  {"id": "rv2", "name": "Romantic 7580", "icon": "https://cdn-icons-png.flaticon.com/128/video_icon.png", "price": 7580, "type": "romantic"},
-  {"id": "rv3", "name": "Romantic 6660", "icon": "https://cdn-icons-png.flaticon.com/128/video_icon.png", "price": 6660, "type": "romantic"},
-  {"id": "rv4", "name": "Romantic 8770", "icon": "https://cdn-icons-png.flaticon.com/128/video_icon.png", "price": 8770, "type": "romantic"},
-  {"id": "rv5", "name": "Romantic 4870", "icon": "https://cdn-icons-png.flaticon.com/128/video_icon.png", "price": 4870, "type": "romantic"}
-]; // এখানে সেমিকোলন আর ব্র্যাকেট ঠিক করে দেওয়া হয়েছে
+  {"id": "r1", "name": "Ring", "icon": "https://cdn-icons-png.flaticon.com/128/4359/4359295.png", "price": 100, "type": "romantic", "videoUrl": ""},
+  {"id": "r2", "name": "Dinner", "icon": "https://cdn-icons-png.flaticon.com/128/2904/2904973.png", "price": 250, "type": "romantic", "videoUrl": ""},
+  
+  {"id": "rv1", "name": "Romantic 9978", "icon": "https://cdn-icons-png.flaticon.com/128/video_icon.png", "price": 9978, "type": "romantic", "videoUrl": "https://raw.githubusercontent.com/robelmiah2692-bit/pagla-chat/main/assets/videos/9978.mp4"},
+  {"id": "rv2", "name": "Romantic 7580", "icon": "https://cdn-icons-png.flaticon.com/128/video_icon.png", "price": 7580, "type": "romantic", "videoUrl": "https://raw.githubusercontent.com/robelmiah2692-bit/pagla-chat/main/assets/videos/7580.mp4"},
+  {"id": "rv3", "name": "Romantic 6660", "icon": "https://cdn-icons-png.flaticon.com/128/video_icon.png", "price": 6660, "type": "romantic", "videoUrl": "https://raw.githubusercontent.com/robelmiah2692-bit/pagla-chat/main/assets/videos/6660.mp4"},
+  {"id": "rv4", "name": "Romantic 8770", "icon": "https://cdn-icons-png.flaticon.com/128/video_icon.png", "price": 8770, "type": "romantic", "videoUrl": "https://raw.githubusercontent.com/robelmiah2692-bit/pagla-chat/main/assets/videos/8770.mp4"},
+  {"id": "rv5", "name": "Romantic 4870", "icon": "https://cdn-icons-png.flaticon.com/128/video_icon.png", "price": 4870, "type": "romantic", "videoUrl": "https://raw.githubusercontent.com/robelmiah2692-bit/pagla-chat/main/assets/videos/4870.mp4"}
+];
