54bb0c6bfc8f3cd76da57bfe90aa0c493412cfc3
robelmiah2692-bit <robelmiah2692@gmail.com>
2026-03-14 07:55:22 +0400

    Update romantic_gifts.dart

--- lib/data/romantic_gifts.dart
+++ lib/data/romantic_gifts.dart
@@ -2,3 +2,11 @@ final List<Map<String, dynamic>> romanticGifts = [
   {"id": "r1", "name": "Ring", "icon": "https://cdn-icons-png.flaticon.com/128/4359/4359295.png", "price": 100, "type": "romantic"},
   {"id": "r2", "name": "Dinner", "icon": "https://cdn-icons-png.flaticon.com/128/2904/2904973.png", "price": 250, "type": "romantic"},
 ];
+
+  // নতুন ৫টি ভিডিও গিফট নিচে যোগ করা হলো
+  {"id": "rv1", "name": "Romantic 9978", "icon": "https://cdn-icons-png.flaticon.com/128/video_icon.png", "price": 9978, "type": "romantic"},
+  {"id": "rv2", "name": "Romantic 7580", "icon": "https://cdn-icons-png.flaticon.com/128/video_icon.png", "price": 7580, "type": "romantic"},
+  {"id": "rv3", "name": "Romantic 6660", "icon": "https://cdn-icons-png.flaticon.com/128/video_icon.png", "price": 6660, "type": "romantic"},
+  {"id": "rv4", "name": "Romantic 8770", "icon": "https://cdn-icons-png.flaticon.com/128/video_icon.png", "price": 8770, "type": "romantic"},
+  {"id": "rv5", "name": "Romantic 4870", "icon": "https://cdn-icons-png.flaticon.com/128/video_icon.png", "price": 4870, "type": "romantic"},
+];
