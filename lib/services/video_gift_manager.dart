75f1b184e5e6aec7f27bcf248aec7437a87adc6e
robelmiah2692-bit <robelmiah2692@gmail.com>
2026-03-14 10:08:03 +0400

    Update video_gift_manager.dart

--- lib/services/video_gift_manager.dart
+++ lib/services/video_gift_manager.dart
@@ -6,38 +6,64 @@ class VideoGiftManager {
   static void playGift(BuildContext context, String videoUrl) {
     if (videoUrl.isEmpty) return;
 
-    final overlayState = Overlay.of(context);
-    late OverlayEntry overlayEntry;
-    
-    // networkUrl এবং Uri.parse ব্যবহার করা হয়েছে
-    VideoPlayerController controller = VideoPlayerController.networkUrl(Uri.parse(videoUrl));
+    // ওয়েবে ওভারলে অনেক সময় ঝামেলা করে, তাই আমরা সরাসরি একটি ডায়ালগ ব্যবহার করবো 
+    // যা ওয়েবে বেশি স্ট্যাবল
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      barrierColor: Colors.black.withOpacity(0.7),
+      builder: (BuildContext context) {
+        return _VideoPlayerWidget(url: videoUrl);
+      },
+    );
+  }
+}
 
-    controller.initialize().then((_) {
-      overlayEntry = OverlayEntry(
-        builder: (context) => Material(
-          color: Colors.transparent,
-          child: Center(
-            child: IgnorePointer(
-              child: AspectRatio(
-                aspectRatio: controller.value.aspectRatio,
-                child: VideoPlayer(controller),
-              ),
-            ),
-          ),
-        ),
-      );
+class _VideoPlayerWidget extends StatefulWidget {
+  final String url;
+  const _VideoPlayerWidget({required this.url});
 
-      overlayState.insert(overlayEntry);
-      controller.play();
+  @override
+  State<_VideoPlayerWidget> createState() => _VideoPlayerWidgetState();
+}
 
-      Future.delayed(controller.value.duration, () {
-        if (overlayEntry.mounted) {
-          controller.dispose();
-          overlayEntry.remove();
+class _VideoPlayerWidgetState extends State<_VideoPlayerWidget> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.networkUrl(Uri.parse(widget.url))
+      ..initialize().then((_) {
+        if (mounted) {
+          setState(() {});
+          _controller.play();
         }
       });
-    }).catchError((error) {
-      debugPrint("Video Error: $error");
+
+    _controller.addListener(() {
+      if (_controller.value.position >= _controller.value.duration) {
+        if (mounted) Navigator.of(context).pop();
+      }
     });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!_controller.value.isInitialized) {
+      return const Center(child: CircularProgressIndicator(color: Colors.pink));
+    }
+    return Center(
+      child: AspectRatio(
+        aspectRatio: _controller.value.aspectRatio,
+        child: VideoPlayer(_controller),
+      ),
+    );
   }
 }
