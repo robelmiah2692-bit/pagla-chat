ad574005fa8c3a1be61fa798fb1dc7e5c105ad28
robelmiah2692-bit <robelmiah2692@gmail.com>
2026-03-19 21:36:57 +0400

    Update stories_service.dart

--- lib/stories_service.dart
+++ lib/stories_service.dart
@@ -2,7 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/foundation.dart';
-import 'dart:io';
+import 'package:universal_html/html.dart' as html;
 
 class StoriesService {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
