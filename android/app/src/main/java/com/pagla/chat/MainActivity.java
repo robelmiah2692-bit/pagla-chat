20a5bd3841a0790b831917f6e9079d2108f830cf
robelmiah2692-bit <robelmiah2692@gmail.com>
2026-03-20 10:54:21 +0400

    Create MainActivity.java

---
+++ android/app/src/main/java/com/pagla/chat/MainActivity.java
@@ -0,0 +1,8 @@
+package com.pagla.chat;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+    // এখানে বাড়তি কোনো কোড দেওয়ার প্রয়োজন নেই, 
+    // ফ্লাটার নিজেই বাকিটা হ্যান্ডেল করবে।
+}
