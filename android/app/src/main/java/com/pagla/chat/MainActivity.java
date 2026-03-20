e2ecbe46147bd593ad3158d8b48bf13675639f13
robelmiah2692-bit <robelmiah2692@gmail.com>
2026-03-20 23:13:38 +0400

    Update MainActivity.java

--- android/app/src/main/java/com/pagla/chat/MainActivity.java
+++ android/app/src/main/java/com/pagla/chat/MainActivity.java
@@ -1,14 +1,8 @@
 package com.pagla.chat;
 
-import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity;
-import io.flutter.embedding.engine.FlutterEngine;
-import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
-    @Override
-    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
-        // এটি আপনার সব প্লাগইনকে সঠিকভাবে অ্যান্ড্রয়েডের সাথে লিঙ্ক করবে
-        GeneratedPluginRegistrant.registerWith(flutterEngine);
-    }
+    // এখানে আর কিছুই লাগবে না। 
+    // ফ্লাটার এখন অটোমেটিক প্লাগইন চিনে নেয়।
 }
