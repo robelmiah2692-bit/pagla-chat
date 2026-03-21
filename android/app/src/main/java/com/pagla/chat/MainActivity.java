15fa22dbba5503c1029014db702e9ad2a678411e
robelmiah2692-bit <robelmiah2692@gmail.com>
2026-03-21 04:16:16 +0400

    Delete android/app/src/main/java/com/pagla/chat/MainActivity.java

--- android/app/src/main/java/com/pagla/chat/MainActivity.java
+++
@@ -1,8 +0,0 @@
-package com.pagla.chat;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-    // এখানে আর কিছুই লাগবে না। 
-    // ফ্লাটার এখন অটোমেটিক প্লাগইন চিনে নেয়।
-}
