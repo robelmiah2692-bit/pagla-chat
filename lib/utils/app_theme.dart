697b64f670f9ce445fa7c292f93435815af72ce0
robelmiah2692-bit <robelmiah2692@gmail.com>
2026-03-26 21:57:59 +0400

    Create app_theme.dart

---
+++ lib/utils/app_theme.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:pagla_chat/utils/app_theme.dart'; // থিম ফাইল ইমপোর্ট করুন
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pagla Chat',
+      debugShowCheckedModeBanner: false,
+      
+      // এই এক লাইনেই পুরো অ্যাপের ডিজাইন বদলে যাবে!
+      theme: AppTheme.darkTheme, 
+      
+      home: const SplashScreen(), // আপনার স্টার্ট পেজ
+    );
+  }
+}
