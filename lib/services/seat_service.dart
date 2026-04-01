b85c2ceb5aee61a9f41949882a6caaf7b73f5346
robelmiah2692-bit <robelmiah2692@gmail.com>
2026-04-01 15:56:52 +0400

    Update seat_service.dart

--- lib/services/seat_service.dart
+++ lib/services/seat_service.dart
@@ -1,4 +1,4 @@
-/*import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class SeatService {
   final FirebaseFirestore _db = FirebaseFirestore.instance;
