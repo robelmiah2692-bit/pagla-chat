e4501a5c8cfb2d8714cd26a1151e058062b5d3e3
robelmiah2692-bit <robelmiah2692@gmail.com>
2026-04-01 16:09:33 +0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,9 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 
 // আপনার তৈরি করা অন্যান্য পেজ
 import 'home_page.dart';
-//import 'screens/voice_room.dart';
+import 'screens/voice_room.dart';
 import 'inbox_page.dart';
-//import 'profile_page.dart';
+import 'profile_page.dart';
 import 'room_list_page.dart';
 
 // ফায়ারবেস কনফিগারেশন
