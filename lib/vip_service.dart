0506d20cc55b941e469e2fab34ae32e5c28ba7fd
robelmiah2692-bit <robelmiah2692@gmail.com>
2026-04-01 05:33:07 +0400

    Update vip_service.dart

--- lib/vip_service.dart
+++ lib/vip_service.dart
@@ -1,4 +1,4 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+/*import 'package:cloud_firestore/cloud_firestore.dart';
 
 class VipService {
   static final FirebaseFirestore _firestore = FirebaseFirestore.instance;
