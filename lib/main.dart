1081c29840a5a6bbe1a2e5a07d53924e059c8f97
robelmiah2692-bit <robelmiah2692@gmail.com>
2026-04-09 10:42:15 +0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -225,6 +225,7 @@ class _CreateProfilePageState extends State<CreateProfilePage> {
         'age': _ageController.text.trim(),
         'gender': _selectedGender,
         'email': user?.email,
+        'uid': user?.uid,
         'authUID': user?.uid, 
         'diamonds': 200,      // শুরুতে ফিক্সড ডাইমন্ড
         'vip_xp': 0,         // VIP XP যোগ করা হলো
